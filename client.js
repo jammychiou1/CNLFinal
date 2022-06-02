39664465eda34ca183d23c55e9d208d099d4e085
jammychiou1 <jammy.chiou1@gmail.com>
2022-06-02 19:37:04 +0800

    removed some settings

--- client.js
+++ client.js
@@ -5,7 +5,6 @@ var dc = null;
 
 function negotiate() {
     pc.addTransceiver('video', {direction: 'recvonly'});
-    pc.addTransceiver('audio', {direction: 'recvonly'});
     return pc.createOffer().then(function(offer) {
         return pc.setLocalDescription(offer);
     }).then(function() {
@@ -49,74 +48,54 @@ function start() {
         sdpSemantics: 'unified-plan'
     };
 
-    if (document.getElementById('use-stun').checked) {
-        config.iceServers = [{urls: ['stun:stun.l.google.com:19302']}];
-    }
+    config.iceServers = [{urls: ['stun:stun.l.google.com:19302']}];
 
     pc = new RTCPeerConnection(config);
 
-    // connect audio / video
+    // connect video
     pc.addEventListener('track', function(evt) {
         if (evt.track.kind == 'video') {
             document.getElementById('video').srcObject = evt.streams[0];
-        } else {
-            document.getElementById('audio').srcObject = evt.streams[0];
         }
     });
 
-    if (document.getElementById('use-datachannel').checked) {
-        var parameters = JSON.parse(document.getElementById('datachannel-parameters').value);
+    var parameters = {"ordered": false, "maxPacketLifetime": 500};
 
-        dc = pc.createDataChannel('keyboard', parameters);
-        // player1
-        let keyMap1 = new Map([ 
-            ['w', 'W'],
-            ['a', 'A'],
-            ['s', 'S'],
-            ['d', 'D'],
-            ['b', 'B'],
-            ['v', 'V']
-        ]);
-        // player2
-        let keyMap2 = new Map([ 
-            ['ArrowLeft', 'Left'],
-            ['ArrowRight', 'Right'],
-            ['ArrowUp', 'Up'],
-            ['ArrowDown', 'Down'],
-            ['.', 'period'],
-            [',', 'comma']
-        ]);
-        let keyMap = document.getElementById('player-id').value == '1' ? keyMap1 : keyMap2;
-        document.onkeydown = function(e) {
-            console.log(e.key);
-            if (keyMap.has(e.key)) {
-                dc.send('keydown' + keyMap.get(e.key));
-            }
-        };
-        document.onkeyup = function(e) {
-            if (keyMap.has(e.key)) {
-                dc.send('keyup' + keyMap.get(e.key));
-            }
-        };
-    }
+    dc = pc.createDataChannel('keyboard', parameters);
+    // player1
+    var keyMap1 = new Map([ 
+        ['w', 'W'],
+        ['a', 'A'],
+        ['s', 'S'],
+        ['d', 'D'],
+        ['b', 'B'],
+        ['v', 'V']
+    ]);
+    // player2
+    var keyMap2 = new Map([ 
+        ['ArrowLeft', 'Left'],
+        ['ArrowRight', 'Right'],
+        ['ArrowUp', 'Up'],
+        ['ArrowDown', 'Down'],
+        ['.', 'period'],
+        [',', 'comma']
+    ]);
+    var keyMap = document.getElementById('player-id').value == '1' ? keyMap1 : keyMap2;
+    document.onkeydown = function(e) {
+        console.log(e.key);
+        if (keyMap.has(e.key)) {
+            dc.send('keydown' + keyMap.get(e.key));
+        }
+    };
+    document.onkeyup = function(e) {
+        if (keyMap.has(e.key)) {
+            dc.send('keyup' + keyMap.get(e.key));
+        }
+    };
+
     document.getElementById('player-id').style.display = 'none';
     document.getElementById('player-id-label').style.display = 'none';
 
     document.getElementById('start').style.display = 'none';
     negotiate();
-    document.getElementById('stop').style.display = 'inline-block';
-}
-
-function stop() {
-    document.getElementById('stop').style.display = 'none';
-
-    // close data channel
-    if (dc) {
-        dc.close();
-    }
-
-    // close peer connection
-    setTimeout(function() {
-        pc.close();
-    }, 500);
 }
